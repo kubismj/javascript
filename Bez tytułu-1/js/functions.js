80e2cd9405954cda6a9c58663e03a0213c796813
kubismj <kubis.m.j@gmail.com>
2017-08-19 16:59:43 +0200

    functions

---
+++ Bez tytułu-1/js/functions.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function multiply(param1, param2, param3) {
+	var result = param1 * param2 * param3;
+	return result;
+}
+
+//multiply(3, 9, 10);
+//multiply(3, 9, 1);
+//multiply(2, 9, 10);
+//multiply(2, 19, 10);
+
+var wynik = multiply(3, 9, 10)
+console.log(wynik);

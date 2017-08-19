60d28c975f541776af33476a24e40de682368198
kubismj <kubis.m.j@gmail.com>
2017-08-19 14:34:08 +0200

    objects

--- Bez tytułu-1/js/objects.js
+++ Bez tytułu-1/js/objects.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var osoba = {
+/* 
+var wojtek = {
 	imie: "Wojtek",
 	wzrost: 181,
 	przedstawOsobe: function() {
@@ -9,17 +10,67 @@ var osoba = {
 }
 // this. odwołuje sie ezpośrednio do imię w danym osoba 
 
-console.log(osoba.imie);
-console.log(osoba.wzrost);
+var kaja = {
+	imie: "Kaja",
+	wzrost: 181,
+	przedstawOsobe: function() {
+		console.log('Cześć jestem ' + this.imie + ', mam ' + this.wzrost + ' wzrostu.');
+	}
+}
 
-osoba.imie = 'Adam'
+console.log(wojtek.imie);
+console.log(wojtek.wzrost);
 
-console.log(osoba['imie']);
-console.log(osoba['wzrost']);
+wojtek.imie = 'Adam'
 
-osoba.przedstawOsobe()
+console.log(wojtek['imie']);
+console.log(wojtek['wzrost']);
+
+wojtek.przedstawOsobe()
 //w przedstawOsobe jest wyswietlenie console.log('Cześć') więc wystarczy się odwołac do przedstawOsobe
-console.log(osoba.przedstawOsobe());
-//jezeli wpiszemy jak powyżej najpierw wyświetli odwołanie a potem samo głóne console.log(..) które jest już puste ponieważ juz yło wywołane
+console.log(wojtek.przedstawOsobe());
+//jezeli wpiszemy jak powyżej najpierw wyświetli odwołanie a potem samo główne console.log(..) które jest już puste ponieważ juz było wywołane
 
 console.log();
+
+console.log(kaja.imie);
+console.log(kaja.wzrost); 
+*/
+
+class Osoba {
+	constructor(imie, nazwisko) {
+		this.imie = imie;
+		this.nazwisko = nazwisko;
+		this.wzrost;
+		this.oczy;
+	}
+	wyswietlInfo() {
+		console.log('Imię: ' + this.imie + "\n"
+					+ 'Nazwisko: ' + this.nazwisko + "\n"
+					+ 'Wzrost: ' + this.wzrost + "\n"
+					+ 'Oczy: ' + this.oczy + "\n");
+	}
+}
+
+// "\n" - enter
+// constructor()  - przypisuje do Osoby zawartość {..}
+// constructor ma prarametry w (imie, nazwisko. ....)
+
+
+var wojtek = new Osoba('Wojtek', 'Potocki');
+
+wojtek.wzrost = 180;
+wojtek.oczy = 'niebieskie';
+
+console.log(wojtek);
+
+var marek = new Osoba('Marek', 'Kubiś');
+
+marek.wzrost = 190;
+marek.oczy = 'szare';
+
+console.log(marek);
+
+
+wojtek.wyswietlInfo();
+marek.wyswietlInfo();

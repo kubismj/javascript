1201aaa770e2f09f1e203412cb8ebfd12f1a6520
kubismj <kubis.m.j@gmail.com>
2017-08-19 10:39:27 +0200

    datatypes

---
+++ Bez tytułu-1/js/datatypes.js
@@ -0,0 +1,31 @@
+'use strict'
+
+/* Typ liczbowy */
+
+var wyplata = 5000;
+var premia = 2500;
+var calkowitaWyplata = wyplata + premia;
+//var calkowitaWyplata = "Zarabiam:  " + (wyplata + premia);
+
+console.log(calkowitaWyplata);
+
+/* Typ - lancuch znakow - string */
+
+var wyplataStr = "5000";
+var premiaStr = "2500";
+var calkowitaWyplataStr = wyplataStr + premiaStr;
+//var calkowitaWyplataStr = "Zarabiam:  " + wyplataStr + " " + premiaStr;
+
+console.log(calkowitaWyplataStr);
+
+/* Typ logiczny - boolean */
+
+var smog = true;
+console.log(smog);
+
+/* Typ specjalne - undefined / null */
+var niezdefiniowanaZmienna;
+var nullowaZmienna = null;
+
+console.log(niezdefiniowanaZmienna);
+console.log(nullowaZmienna);
